b3bebbd168542cba4e1b345b09d3816143c5c87b
victor-teodozio <victorteodozio@hotmail.com>
2020-06-07 00:47:35 -0300

    Iniciando o projeto no STS

---
+++ src/main/java/demo/ClinicaVeterinariaApplication.java
@@ -0,0 +1,18 @@
+package demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaVeterinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaVeterinariaApplication.class, args);
+		System.out.println("Funcionando...");
+
+	}
+
+}
+
+
